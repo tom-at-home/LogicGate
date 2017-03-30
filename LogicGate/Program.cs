8851ea67c1fb205c98f2e310630e8622cc09ebb0
Tomasz <tomasz.urbaniak@me.com>
2017-03-30 19:01:33 +0200

    Add Xor Gate to Program

--- LogicGate/Program.cs
+++ LogicGate/Program.cs
@@ -32,9 +32,13 @@ namespace LogicGate
             and2.ConnectInput1(new TrueProducer());
             and2.ConnectInput2(new TrueProducer());
 
+            Xor xor1 = new Xor();
+            xor1.ConnectInput1(new TrueProducer());
+            xor1.ConnectInput2(new TrueProducer());
+
             And and3 = new And();
             and3.ConnectInput1(and1);
-            and3.ConnectInput2(and2);
+            and3.ConnectInput2(xor1);
 
             Receiver rec = new Receiver();
             rec.ConnectInput1(and3);
